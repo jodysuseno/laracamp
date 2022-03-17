52a43033094b1995505694d48d065ffa15f18bd9
Jody Ririt Krido Suseno <jodysuseno@gmail.com>
2022-03-18 00:41:29 +0700

    create discount feature

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -8,6 +8,7 @@ use App\Models\Camp;
 use Illuminate\Http\Request;
 use App\http\Requests\User\Checkout\Store;
 use App\Mail\Checkout\AfterCheckout;
+use App\Models\Discount;
 use Auth;
 use Mail;
 use Exception;
@@ -68,6 +69,12 @@ class CheckoutController extends Controller
         $user->phone = $data['phone'];
         $user->address = $data['address'];
         $user->save();
+
+        if ($request->discount) {
+            $discount = Discount::whereCode($request->discount)->first();
+            $data['discount_id'] = $discount->id;
+            $data['discount_percentage'] = $discount->percentage;
+        }
 
         $checkout =  Checkout::create($data);
 
@@ -134,16 +141,29 @@ class CheckoutController extends Controller
 
         $checkout->midtrans_booking_code = $orderId;
 
-        $transaction_details = [
-            'order_id' => $orderId,
-            'gross_amount' => $price
-        ];
 
         $item_details[] = [
             'id' => $orderId,
             'price' => $price,
             'quantity' => 1,
             'name' => "Payment for {$checkout->Camp->title} Camp"
+        ];
+
+        $discountPrice = 0;
+        if ($checkout->Discount) {
+            $discountPrice = $price * $checkout->discount_percentage / 100;
+            $item_details[] = [
+                'id' => $checkout->Discount->code,
+                'price' => -$discountPrice,
+                'quantity' => 1,
+                'name' => "Discount {$checkout->Discount->name} ({ $checkout->discount_percentage }%)"
+            ];
+        }
+
+        $total = $price - $discountPrice;
+        $transaction_details = [
+            'order_id' => $orderId,
+            'gross_amount' => $total
         ];
 
         $userData = [
@@ -175,6 +195,7 @@ class CheckoutController extends Controller
             // Get Snap Payment Page URL
             $paymentUrl = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
             $checkout->midtrans_url = $paymentUrl;
+            $checkout->total = $total;
             $checkout->save();
 
             return $paymentUrl;
--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -12,7 +12,7 @@ class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['user_id','camp_id','payment_status', 'midtrans_url', 'midtrans_booking_code'];
+    protected $fillable = ['user_id','camp_id','payment_status', 'midtrans_url', 'midtrans_booking_code','discount_id', 'discount_percentage', 'total'];
 
     public function setExpiredAttribute($value){
         $this->attributes['expired'] = date('Y-m-t', strtotime($value));
@@ -24,5 +24,9 @@ class Checkout extends Model
 
     public function User(): BelongsTo{
         return $this->belongsTo(User::class);
+    }
+
+    public function Discount(): BelongsTo{
+        return $this->belongsTo(Discount::class);
     }
 }
